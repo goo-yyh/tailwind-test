8815a088dc8f7a1f31b9878a9a07a2a0ca462ade
yyh <m18758428901@163.com>
2023-02-01 11:29:00 +0800

    feat: init

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Page() {
+  return (
+    <div className="text-amber-50">
+      <h1 className="text-red-300">Page index</h1>
+    </div>
+  );
+}
